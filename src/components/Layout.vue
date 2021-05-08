eca0f5a2de786cfdeab9790454837fc0d46cd2a0
Jariel Floriano <jariel.floriano@gmail.com>
2021-05-08 16:26:01 -0300

    Init

---
+++ src/components/Layout.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="nav">
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link>
+  </div>
+  <router-view />
+</template>
+
+<script>
+export default {
+  name: "Layout"
+}
+</script>
+
+<style scoped>
+
+</style>

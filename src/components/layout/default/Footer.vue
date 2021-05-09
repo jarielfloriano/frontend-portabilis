e8b98557e09ed849eb83d96c821ffc770b67e003
Jariel Floriano <jariel.floriano@gmail.com>
2021-05-08 23:12:32 -0300

    fix(layout): Corrigido links de navegação

--- src/components/layout/default/Footer.vue
+++ src/components/layout/default/Footer.vue
@@ -1,11 +1,9 @@
 <template>
   <div class="w-full border-t-2 px-8 py-6 lg:flex justify-between items-center">
-    <p class="mb-2 lg:mb-0">© Copyright 2020</p>
+    <p class="mb-2 lg:mb-0">© Copyright 2021</p>
 
     <div class="flex">
-      <a href="#" class="mr-6 hover:text-gray-900">Terms of Service</a>
-      <a href="#" class="mr-6 hover:text-gray-900">Privacy Policy</a>
-      <a href="#" class="hover:text-gray-900">About Us</a>
+     Aplicação destinada a avaliação técnica. Feito com &#128153; by Jariel Floriano
     </div>
   </div>
 </template>

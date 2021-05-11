d3648096f674b2f8461c31db457eed933678a89d
Jariel Floriano <jariel.floriano@gmail.com>
2021-05-10 21:25:12 -0300

    fix(store): Alterdo store de forma simples e funcional

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import { createApp } from 'vue';
 import App from './App.vue';
 import router from "./router";
-import {store} from './store';
+import store from './store/simple';
 import './assets/index.css';
 
 const app = createApp(App)
